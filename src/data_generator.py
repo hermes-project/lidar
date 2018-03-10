075d933498725590d11d33754d91edf0ba4c5bf0
Florian <eclipse.florian@gmail.com>
2018-03-10 19:18:12 +0100

    traitement du dico pour calcul de moyenne

--- src/data_generator.py
+++ src/data_generator.py
@@ -15,15 +15,33 @@ def generator(lidar, resolution):
     data = {}
     i = 0
     arround = resolution * 10.
-    while alpha <= 360.: #génération des clés dans le dictionnaire
+    while alpha < 360.: #génération des clés dans le dictionnaire
         data[alpha] = []
-        alpha += resolution
+        alpha = round((alpha+resolution), 1)
 
     for measure in lidar.iter_measures():
         if measure[0]: # si à TRUE (ie nouveau tour) on incremente
             i += 1
-        theta = round(measure[2]/arround,1)*arround # arrondie a la resolution près. EX : à 0.5 près pour 2,57 et 2,8. round(2,57 / 5 , 1) = 0.5 et 0.5 * 5 = 2.5 . round ( 2,8 / 5 , 1) = 0.6 et 0.6 * 5 = 3
+        theta = round(measure[2]/arround, 1)*arround # arrondie a la resolution près. EX : à 0.5 près pour 2,57 et 2,8. round(2,57 / 5 , 1) = 0.5 et 0.5 * 5 = 2.5 . round ( 2,8 / 5 , 1) = 0.6 et 0.6 * 5 = 3
         data[theta].append(measure[3]) # on ajoute la data dans l'array dont la clé correspond à l'angle
         if i >= 10 : # si 10 tours realise
             break
+
+    for angle,distances in data.items():
+        j = 0
+        m = average(distances)
+        et = standard_deviation(distances,m)
+        while j < len(distances) :
+            if (distances < m-(3*et) or distances > m + (3*et)):
+                distances.pop(j)
+            else:
+                j +=1
+        data[angle] = average(distances)
     return data
+
+def average(array):
+    return sum(array, 0.0) / len(array)
+
+def standard_deviation(array,m):
+    return (average([(x - m)**2 for x in array]))**0.5
+
