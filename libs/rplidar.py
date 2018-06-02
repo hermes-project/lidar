d7697a40d5566460e02b145da82e314f09fb0069
tictacmenthe <ugurcan.ekren@telecom-sudparis.eu>
2018-06-02 02:02:43 +0200

    pour willie

--- libs/rplidar.py
+++ libs/rplidar.py
@@ -449,13 +449,10 @@ class RPLidar(object):
         '''
         scan_list = []
         iterator = self.iter_measures(scan_type, max_buf_meas)
-        t=time.time()
         for new_scan, quality, angle, distance in iterator:
 
             if new_scan:
                 if len(scan_list) > min_len:
-                    print(time.time() - t)
-                    t = time.time()
                     yield scan_list
                 scan_list = []
             if distance > 0:
